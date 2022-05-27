74ea0a082b04652c4df32a893cb203c6735e2337
Aarush Shintre <65198209+AarushShintre@users.noreply.github.com>
2022-05-27 17:29:20 +0530

    hangman game in py

---
+++ hangmanGame/hangman.py
@@ -0,0 +1,58 @@
+# hangman.py
+#hangman game implementation in python
+
+import random
+
+wordList = [
+    ("keyboard", "Used to Type")  , 
+    ("mouse","Used to scroll"),
+    ("laptop","Used to code"),
+    ("table","a piece of furniture with a flat top used for writing"),
+    ("chair","Something you sit on"),
+    ("headphones","Used to listen to music"),
+]
+
+chances = 1
+
+exit = "y"
+while exit != "n":
+    while chances > 0 :
+        randomChoice = random.randrange(len(wordList))
+        word = wordList[randomChoice][0]
+        if " " in word:
+            word = word.split(" ")[0] + "/"+word.split(" ")[1]
+        chances = len(word) +1
+        blankList = ["_"] *len(word)
+        print()
+        print("HINT: ", wordList[randomChoice][1])
+        for i in range(chances):
+            for i in blankList:
+                print(i, end=" ")
+            print()
+            print()
+            print("There are ", chances, "chances left.")
+            print()
+            guess = input("Please enter a letter to guess! \n")
+            print()
+            numOccurence = word.count(guess)
+            wordSliced = word
+            lengthSlicedOut = 0
+            for i in range(numOccurence):
+                blankList[wordSliced.find(guess) + lengthSlicedOut] = guess
+                lengthSlicedOut += len(word[:numOccurence+1])
+                if numOccurence >1:
+                    wordSliced = word[numOccurence+1:]
+            if blankList == list(word):
+                print("Correct! The word was ",word )
+                break
+            
+            chances = chances-1  
+
+    if blankList != list(word):
+        print("Oops! The word was ",word )
+        print("Try again!")
+    exit = input("Do you wish to play again?(y/n)")
+    chances = 1
+
+                
+
